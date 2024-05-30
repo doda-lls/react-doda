27dd8b0d0489f7d52315a3cc305fcb099ef40a72
doda-lls <Danielle.Oda@lls.edu>
2024-05-30 16:42:33 -0700

    Added inline CSS styles to Heading.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Heading />
+        <Heading firstName="Danielle" />
+        <Heading firstName="Bob" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,5 +1,9 @@
-function Heading() {
-    return <h1>Header goes here</h1>
+function Heading(props) {
+  const headingStyle = {
+    color: "blue",
+    fontFamily: "times",
+  }
+    return <h1 style={headingStyle}>Header {props.firstName}</h1>
   }
   
 export default Heading;
