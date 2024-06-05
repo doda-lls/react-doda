2579611efafae7aec795f5e57edddbc0a8bbe1be
doda-lls <Danielle.Oda@lls.edu>
2024-06-05 16:57:02 -0700

    Added Card.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Heading from "./components/Heading";
+import Card from "./components/Card";
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <header className="App-header">
         <Heading firstName="Danielle" />
         <Heading firstName="Bob" />
+        <Card h2="First card's h2" h3="First card's h3" />
+        <Card h2="Second card's h2" h3="Second card's h3" />
+        <Card h2="Third card's h2" h3="Third card's h3" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
